52be49116d69e44388f236e12d7e3a8be0cd2600
菅原 浩 <sncts35326@gmail.com>
2018-01-14 08:49:40 +0900

    display generated image for preview feature

--- src/main/scala/jp/_5000164/slack_emoji_generator/Main.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/Main.scala
@@ -2,8 +2,9 @@ package jp._5000164.slack_emoji_generator
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.image.{Image, ImageView}
+import javafx.scene.image.{ImageView, WritableImage}
 import javafx.scene.layout.StackPane
+import javafx.scene.paint.Color
 import javafx.stage.Stage
 
 object Main extends App {
@@ -12,8 +13,13 @@ object Main extends App {
 
 class Main extends Application {
   override def start(primaryStage: Stage): Unit = {
-    val image = new Image(getClass.getResource("/test.png").toString)
-    val imageView = new ImageView(image)
+    val wi = new WritableImage(300, 300)
+    val pw = wi.getPixelWriter
+    for {
+      x <- 0 until 300
+      y <- 0 until 300
+    } pw.setColor(x, y, Color.BLACK)
+    val imageView = new ImageView(wi)
     val root = new StackPane()
     root.getChildren.add(imageView)
     val scene = new Scene(root, 350, 350)

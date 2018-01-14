00e2ae287579e2fe13fb23372b29b40e277f57c9
菅原 浩 <sncts35326@gmail.com>
2018-01-14 09:21:14 +0900

    change to use canvas because draw text

--- src/main/scala/jp/_5000164/slack_emoji_generator/Main.scala
+++ src/main/scala/jp/_5000164/slack_emoji_generator/Main.scala
@@ -2,7 +2,7 @@ package jp._5000164.slack_emoji_generator
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.image.{ImageView, WritableImage}
+import javafx.scene.canvas.Canvas
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.stage.Stage
@@ -13,15 +13,12 @@ object Main extends App {
 
 class Main extends Application {
   override def start(primaryStage: Stage): Unit = {
-    val wi = new WritableImage(300, 300)
-    val pw = wi.getPixelWriter
-    for {
-      x <- 0 until 300
-      y <- 0 until 300
-    } pw.setColor(x, y, Color.BLACK)
-    val imageView = new ImageView(wi)
+    val c = new Canvas(300, 300)
+    val gc = c.getGraphicsContext2D
+    gc.setFill(Color.GRAY)
+    gc.fillRect(0, 0, 300, 300)
     val root = new StackPane()
-    root.getChildren.add(imageView)
+    root.getChildren.add(c)
     val scene = new Scene(root, 350, 350)
     primaryStage.setTitle("test")
     primaryStage.setScene(scene)
